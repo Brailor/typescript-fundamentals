74723f98e9541a89ebe71cf99d8929da8dcaeb6d
Viktor Ohad <ygatravaz@protonmail.com>
2019-03-10 17:51:47 +0100

    3-basics

--- challenges/json-types/src/index.ts
+++ challenges/json-types/src/index.ts
@@ -1,1 +1,9 @@
 // 💡 HINT: number[] and Array<number> mean the same thing
+
+export type JSONValue = string | number | boolean | null | JSONObject | JSONArray;
+
+export interface JSONObject {
+  [k: string]: JSONValue;
+}
+
+export interface JSONArray extends Array<JSONValue> {}
